b9982da309a58773d1c813dcf648e0bce940af67
Vlad Morosanu <vladmorosanu97@gmail.com>
2019-04-16 01:18:59 +0300

    refactoring

--- src/core/mainLayout/MainLayout.jsx
+++ src/core/mainLayout/MainLayout.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from "react";
 
 //layout componet
-import Header from '../header/Header';
+import Header from "../header/Header";
 
 export default function MainLayout(props) {
-  console.log(props.location.pathname);
   return (
-    <div style={{ height: '100%' }}>
+    <div style={{ height: "100%" }}>
       <Header page={props.location.pathname} />
       <main className="main-content">{props.children}</main>
     </div>
