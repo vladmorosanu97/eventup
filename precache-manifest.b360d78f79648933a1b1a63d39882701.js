a43162ea71da94b70b85de2c7a171016e8a0045e
Vlad Morosanu <vladmorosanu97@gmail.com>
2019-04-16 02:25:55 +0300

    Updates

---
+++ precache-manifest.b360d78f79648933a1b1a63d39882701.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c46095118380d38f12e67c916b427f9",
+    "url": "/eventup/static/media/Montserrat-Regular.9c460951.ttf"
+  },
+  {
+    "revision": "0a9d5425e2fbb9900fb6",
+    "url": "/eventup/static/css/main.6286bfc7.chunk.css"
+  },
+  {
+    "revision": "63bd91f6391cdef59644",
+    "url": "/eventup/static/js/runtime~main.e20a2fa4.js"
+  },
+  {
+    "revision": "213051212c21492bc1cc",
+    "url": "/eventup/static/js/2.ba5b60f8.chunk.js"
+  },
+  {
+    "revision": "4b0a719428a374b476ac8b44905bcdb7",
+    "url": "/eventup/static/media/night.4b0a7194.mp4"
+  },
+  {
+    "revision": "1800ccce3f4ccf4a3fcd3572beb4f357",
+    "url": "/eventup/static/media/180301_15_B_KowloonPark_06.1800ccce.mp4"
+  },
+  {
+    "revision": "0a9d5425e2fbb9900fb6",
+    "url": "/eventup/static/js/main.60a933d2.chunk.js"
+  },
+  {
+    "revision": "dcfb8ca5a05e878814942e84c9e1b49f",
+    "url": "/eventup/static/media/cover_low.dcfb8ca5.jpg"
+  },
+  {
+    "revision": "5493e286753205d82be6b67cd07829de",
+    "url": "/eventup/static/media/cover_med.5493e286.jpg"
+  },
+  {
+    "revision": "ab6b65fa8cf74f8129fc221f7956ef96",
+    "url": "/eventup/static/media/undraw_empty_xct9.ab6b65fa.svg"
+  },
+  {
+    "revision": "d427f4281ac60121069fa02486073b30",
+    "url": "/eventup/static/media/undraw_browser_stats_704t.d427f428.svg"
+  },
+  {
+    "revision": "1b39f666d9ec4b2d00f562f8b51fb9d3",
+    "url": "/eventup/index.html"
+  }
+];
